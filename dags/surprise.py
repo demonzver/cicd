cffc617b4625645ba9770f8f881cfaf25b41f2fc
demon <demonzvermsu@gmail.com>
2025-02-17 01:36:13 +0100

    Test

--- dags/surprise.py
+++ dags/surprise.py
@@ -7,7 +7,7 @@ from airflow.utils.decorators import apply_defaults
 
 class LogDatesOperator(BaseOperator):
     """
-    Оператор, который выводит логические даты (logical_date, execution_date и т.д.) из контекста Airflow в логи.
+    Оператор, который выводит логические даты и прочие даты (start_date и т. д.) в логи.
     """
 
     @apply_defaults
@@ -15,21 +15,38 @@ class LogDatesOperator(BaseOperator):
         super().__init__(*args, **kwargs)
 
     def execute(self, context):
-        # В современном Airflow (2.3+), основная дата называется logical_date.
-        # Но для совместимости мы выведем и "execution_date".
         self.log.info("=== Airflow Context Logical Dates ===")
+        # Стандартные поля контекста (работают и в новых версиях Airflow)
         self.log.info("logical_date: %s", context.get("logical_date"))
         self.log.info("execution_date: %s", context.get("execution_date"))
-        self.log.info("ds (string formatted logical_date): %s", context.get("ds"))
+        self.log.info("ds: %s", context.get("ds"))
         self.log.info("data_interval_start: %s", context.get("data_interval_start"))
         self.log.info("data_interval_end: %s", context.get("data_interval_end"))
 
-        # Airflow также предоставляет дополнительные переменные, например, ds_nodash, prev_ds, next_ds и т.д.
-        # При необходимости можно их тоже вывести:
+        # Дополнительные поля, часто используемые для ссылок на предыдущую/следующую дату
         self.log.info("prev_ds: %s", context.get("prev_ds"))
         self.log.info("next_ds: %s", context.get("next_ds"))
 
-        # Возвращаем что-нибудь для наглядности (попадёт в XCom)
+        # -------------------------
+        #   Получение START_DATE
+        # -------------------------
+        # 1) start_date DAG-а (указывается при создании DAG)
+        dag_obj = context["dag"]
+        self.log.info("dag.start_date (из определения DAG): %s", dag_obj.start_date)
+
+        # 2) start_date самого DAG Run (когда Airflow фактически начал этот запуск)
+        dag_run_obj = context["dag_run"]
+        self.log.info("dag_run.start_date (время начала DAG Run): %s", dag_run_obj.start_date)
+
+        # 3) start_date TaskInstance (когда таска фактически началась)
+        task_instance_obj = context["task_instance"]
+        self.log.info("task_instance.start_date (время старта задачи): %s", task_instance_obj.start_date)
+
+        # При желании можно вывести все ключи контекста,
+        # но учтите, что там может быть много служебных объектов.
+        # for key, value in context.items():
+        #     self.log.info("context[%s] = %s", key, value)
+
         return "Dates have been logged."
 
 
